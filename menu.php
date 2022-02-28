368009ebd6392b31d2fc7f96d4386efa31c3306a
NildaX <nilda_gaby_9745@live.com.mx>
2022-02-27 21:23:14 -0600

    avance

---
+++ menu.php
@@ -0,0 +1,26 @@
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Minified JS library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilosp.css">
+    <link rel="icon" href="assets/images/logo.png" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600&display=swap" rel="stylesheet">
+</head>
+<body style="background-color:#FFD1DC;"> 
+    <div class="container-fluid px-0">
+            <?php require 'shared/header.php'?>
+    </div> 
+    <br>
+    <div align="center"> 
+        <h1>NUESTRO MENÚ </h1>
+    </div>
+    <br>
+</body>
+</html>
---
+++ shared/header.php
@@ -0,0 +1,13 @@
+<nav style="background-color:#FFFFFF;">
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+          <img src="./assets/images/logo.png" alt="NASAcakes">
+      </div>
+      <div class="col-4">
+              
+      </div>
+    </div>
+  </div>
+  <!-- Collapsible content -->
+</nav>
